75e514711d7684d8bbe634d6875bb76649ce8628
ElJokerxD <edu_ff78@hotmail.com>
2014-10-13 01:16:54 -0600

    pues no

--- MazeGen/WindowsFormsApplication1/Form1.cs
+++ MazeGen/WindowsFormsApplication1/Form1.cs
@@ -17,30 +17,36 @@ namespace WindowsFormsApplication1
         BufferedGraphics myBuffer;
         Game game;
         int cont;
- 
+        bool pasa=false;
+        public static int maxw = Screen.PrimaryScreen.Bounds.Width;
+        public static int maxh = Screen.PrimaryScreen.Bounds.Height;
+        
         public Form1()
         {
             InitializeComponent();
-            cont = 0;
-          
         }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-            
             using (currentContext = BufferedGraphicsManager.Current)
             using (myBuffer = currentContext.Allocate(this.CreateGraphics(), this.DisplayRectangle))
             {
                 game.draw(myBuffer);
-                game.update(myBuffer);
                 myBuffer.Render(this.CreateGraphics());
             }
-            cont++;
 
-            if(cont == 50) 
+            cont++;
+            if(!pasa)
+            {
+                game.Enemymove();
+                pasa = true;
+                
+            }
+            if(cont==2500)
             {
                 cont = 0;
                 game.reset(myBuffer);
+
             }
         }
 
@@ -49,9 +55,10 @@ namespace WindowsFormsApplication1
             game = new Game();
             game.PlayerGenerator();
             game.PlayerSpaw();
+            game.givematriz();
+            game.EnemySpawn();
+            //game.Enemymove();
             cont = 0;
-            
-            
         }
 
         private void Form1_KeyDown(object sender, KeyEventArgs e)
@@ -64,21 +71,7 @@ namespace WindowsFormsApplication1
                 game.PlayerMove(1);
             if (e.KeyCode == Keys.Right)
                 game.PlayerMove(4);
-
-            if (e.KeyCode == Keys.Space)// CREAR BALAS
-            {
-                
-                if (game.cont < 10) //limitar cantidad de balas..10?
-                   
-                {
-                    game.bulletSpawn(1);//crea bala
-                    game.bulletpos();//saca pos del player
-                    game.crea1 = true;
-                    game.cont =game.cont + 1;
-                }
-            }
-          
-                
+            
 
         }
      }
